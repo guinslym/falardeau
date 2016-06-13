ca40fb785eeee52696e5bcefc787ebf9c3bc7fa1
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-06-13 18:07:20 -0400

    adding model

--- applications/movies/models.py
+++ applications/movies/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
+from django.core.urlresolvers import reverse
+import datetime
+from django.utils import timezone
 
-# Create your models here.
+class Film(TimeStampedModel):
+    name = models.CharField(max_length=12, blank=True, unique=True, null=True)
+    screenshot = models.ImageField(upload_to='film/%Y/%m/%d', null=True, blank=True)
+    expiring_date = models.DateField(
+                            default=(timezone.now() + datetime.timedelta(days=30)),
+                            blank=True, null=True,
+                            help_text="release date")
+    def __str__(self):
+        return self.name
+    '''
+    def get_absolute_url(self):
+        return reverse('movies:detail',
+                       args=[str(self.id)]
+                )
+    '''
